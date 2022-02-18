79a43ba60d9857d5fe92a23e3a9d30930ab9a076
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2022-02-18 12:41:56 +0530

    Create optimized.cpp

---
+++ Mathematics/Divisor-of-no/optimized.cpp
@@ -0,0 +1,45 @@
+// Find divisor of a number
+// Time complexity : O(sqrt(n)) 
+
+
+#include<bits/stdc++.h>
+#include<cmath>
+#include<algorithm>
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)
+#define pb v.push_back
+
+using namespace std;
+
+vector<int> divisor_of_no(int n)    // The logic is divisors always appear in pairs
+{
+    vector<int>v;
+    for(int i=1; i*i<=n; i++)      // Running loop till sqrt(n) x*y<=n
+    {
+        if(n%i==0)
+       {
+          pb(i);
+        if(i!=(n/i))
+          pb(n/i);
+       }
+    }
+  return v;
+          
+}
+
+int main(){
+
+  f0;
+  int n;
+  
+  cin>>n;
+  
+  vector<int>res = divisor_of_no(n);
+  
+  for(auto it: res)
+    cout<<it<<" ";
+  cout<<"\n";
+ return 0;
+}
+
+
