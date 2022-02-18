f89ff4e7b584f84d2c0c51e0300beed1678537dd
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2022-02-19 01:21:26 +0530

    Create optimized-sol.cpp

---
+++ Bit-Manipulation/Count-Set-Bits/optimized-sol.cpp
@@ -0,0 +1,29 @@
+// Count number of set bits in the given number
+//Time Complexity : theta(total set bits in n)
+// This solution traverses through only set bits of the number
+
+#include<bits/stdc++.h>
+#include<cmath>
+#include<algorithm>
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)
+
+
+using namespace std;
+
+
+int main()
+{
+   f0;
+   int n,res=0;
+   cin>>n;
+   
+   while(n)
+   {
+      res++;
+      n = n & (n-1);    // removing the last set bit of the given number
+   }
+   
+   cout<<res<<"\n";
+   return 0;
+}
